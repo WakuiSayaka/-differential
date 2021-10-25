41fdd1b3b78eb4f0e1869bb7ce5a29c1fdcab08d
tsumugi <85925233+tsumugiw@users.noreply.github.com>
2021-10-26 04:18:38 +0900

    微分

--- dualnumber/differential.cpp
+++ dualnumber/differential.cpp
@@ -1,71 +1,60 @@
 #include "DualNumber.hpp"
 
+
+// 確認用の関数
+// return exp(x);
+// return exp(2.0*x);
+// return sin(x);
+// return sin(2.0*x);
+// return sin(x) * sin(x);
+// return cos(x);
+// return cos(2.0 * x);
+// return cos(x) * cos(x);
+// return tan(x);
+// return tan(2.0*x);
+// return tan(x)*tan(x);
+// return log(x);
+// return log(2.0*x);
+// return log(x)*log(x);
+// return sqrt(x);
+// return pow(x,-0.5);
+// return pow(x,1.25);
+// return pow(x,-1.25);
+// return sinh(x);
+// return cosh(x);
+// return tanh(x);
+
+
+
+//f(x)
 double func(double x) {
-  double res;
-  res = 4.0*x*x + 2.0*x + 3.0;
-  // res = exp(x);
-  // res = exp(2.0*x);
-  // res = sin(x);
-  // res = sin(2.0*x);
-  // res = sin(x) * sin(x);
-  // res = cos(x);
-  // res = cos(2.0 * x);
-  // res = cos(x) * cos(x);
-  // res = tan(x);
-  // res = tan(2.0*x);
-  // res = tan(x)*tan(x);
-  // res = log(x);
-  // res = log(2.0*x);
-  // res = log(x)*log(x);
-  // res = sqrt(x);
-  // res = pow(x,-0.5);
-  // res = pow(x,1.25);
-  // res = pow(x,-1.25);
-  // res = sinh(x);
-  // res = cosh(x);
-  // res = tanh(x);
-  return res;
+  return 4.0*x*x + 2.0*x + 3.0;
 }
 
+//f(x)
+Matrix func(Matrix x) {
+  return 4.0*x*x + 2.0*x + 3.0;
+}
+
+//f'(x)
 double differential_func(double rx) {
-  Matrix res;
   Matrix x;
-
-  x.DualNumber();
-  x = x + rx;  //ε + x
-
-  res = 4.0*x*x + 2.0*x + 3.0;
-  // res = exp(x);
-  // res = exp(2.0*x);
-  // res = sin(x);
-  // res = sin(2.0*x);
-  // res = sin(x) * sin(x);
-  // res = cos(x);
-  // res = cos(2.0 * x);
-  // res = cos(x) * cos(x);
-  // res = tan(x);
-  // res = tan(2.0*x);
-  // res = tan(x)*tan(x);
-  // res = log(x);
-  // res = log(2.0*x);
-  // res = log(x)*log(x);
-  // res = sqrt(x);
-  // res = pow(x,-0.5);
-  // res = pow(x,1.25);
-  // res = pow(x,-1.25);
-  // res = sinh(x);
-  // res = cosh(x);
-  // res = tanh(x);
-  return res.GetDN();
+  x.DualNumber(); //ε
+  x += rx;  //ε + x
+  
+  return func(x).GetDN();
 }
 
 
 
  // main 関数
 int main(){
-  double x = 2.0;
+  double x;
   double fx,fdx;
   double test;
+
+  //f(x)とf'(x)を出力する為の任意の値xを入力
+  x = 2.0;
 
   fx   = func(x);
   fdx  = differential_func(x);
